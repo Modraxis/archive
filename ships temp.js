678a668b57ebc3b15281f58b8c91048132956470
Basit <155421846+Modraxis@users.noreply.github.com>
2026-06-20 19:38:53 +0500

    Create ships temp.js

---
+++ ships temp.js
@@ -0,0 +1,42 @@
+{
+    101: { name: '', code: 'JSON String...' },
+    ///////////////////
+    201: { name: '', code: 'JSON String...' },
+    202: { name: '', code: 'JSON String...' },
+    ///////////////////
+    301: { name: '', code: 'JSON String...' },
+    302: { name: '', code: 'JSON String...' },
+    303: { name: '', code: 'JSON String...' },
+    304: { name: '', code: 'JSON String...' },
+    ///////////////////
+    401: { name: '', code: 'JSON String...' },
+    402: { name: '', code: 'JSON String...' },
+    403: { name: '', code: 'JSON String...' },
+    404: { name: '', code: 'JSON String...' },
+    405: { name: '', code: 'JSON String...' },
+    406: { name: '', code: 'JSON String...' },
+    ///////////////////
+    501: { name: '', code: 'JSON String...' },
+    502: { name: '', code: 'JSON String...' },
+    503: { name: '', code: 'JSON String...' },
+    504: { name: '', code: 'JSON String...' },
+    505: { name: '', code: 'JSON String...' },
+    506: { name: '', code: 'JSON String...' },
+    507: { name: '', code: 'JSON String...' },
+    508: { name: '', code: 'JSON String...' },
+    ///////////////////
+    601: { name: '', code: 'JSON String...' },
+    602: { name: '', code: 'JSON String...' },
+    603: { name: '', code: 'JSON String...' },
+    604: { name: '', code: 'JSON String...' },
+    605: { name: '', code: 'JSON String...' },
+    606: { name: '', code: 'JSON String...' },
+    607: { name: '', code: 'JSON String...' },
+    608: { name: '', code: 'JSON String...' },
+    609: { name: '', code: 'JSON String...' },
+    ///////////////////
+    701: { name: '', code: 'JSON String...' },
+    702: { name: '', code: 'JSON String...' },
+    703: { name: '', code: 'JSON String...' },
+    704: { name: '', code: 'JSON String...' },
+}
